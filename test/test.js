11fd2498081e1173604ae5161fcfa6d75826bbe5
Sophie <sophie.cooper@7digital.com>
2020-05-11 12:04:02 +0100

    Show jumbled letters in rows

--- test/test.js
+++ test/test.js
@@ -14,6 +14,12 @@ describe('anagram manager', () => {
     return await getProperty(el, 'textContent');
   }
 
+  async function enterNewWord(page, word) {
+      await page.evaluate(() => document.querySelector('input').value = '');
+      await page.type('input', word);
+      await page.click('button');
+  }
+
   before(async () => {
     browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     page = await browser.newPage();
@@ -54,22 +60,82 @@ describe('anagram manager', () => {
     });
 
     it('shows the jumbled word', async () => {
-      const word = await getText(await page.$('div'));
-      assert.notEqual(word, 'SOMEWORD');
-      assert.equal(word.split('').sort().join(''), 'DEMOORSW');
+      const letters = await page.$$('div.row div.letter');
+      const word = await Promise.all(letters.map(async letter => await getText(letter)));
+
+      assert.notEqual(word.join(''), 'SOMEWORD');
+      assert.equal(word.sort().join(''), 'DEMOORSW');
+    });
+
+    it('shows the word over multiple lines', async () => {
+      const letters = await page.$$('div.row');
+      assert.equal(letters.length, 3);
     });
 
     describe('when another word is entered', () => {
-      before(async () => {
-        await page.evaluate(() => document.querySelector('input').value = '');
-        await page.type('input', 'anotherthing');
-        await page.click('button');
-      });
+      before(() => enterNewWord(page, 'anotherthing'));
 
-      it('shows the jumbled word', async () => {
-        const word = await getText(await page.$('div'));
-        assert.notEqual(word, 'ANOTHERTHING');
-        assert.equal(word.split('').sort().join(''), 'AEGHHINNORTT');
+      it('shows the new jumbled word', async () => {
+        const letters = await page.$$('div.row div.letter');
+        const word = await Promise.all(letters.map(async letter => await getText(letter)));
+
+        assert.notEqual(word.join(''), 'ANOTHERTHING');
+        assert.equal(word.sort().join(''), 'AEGHHINNORTT');
+      });
+    });
+
+    [1, 2].forEach(letterCount => {
+      describe(`when a ${letterCount} letter word is entered`, () => {
+        before(() => enterNewWord(page, Array(letterCount).fill('a').join('')));
+
+        it('shows it on one line', async () => {
+          const letters = await page.$$('div.row');
+          assert.equal(letters.length, 1);
+        });
+      });
+    });
+
+    [3, 4, 5, 6].forEach(letterCount => {
+      describe(`when a ${letterCount} letter word is entered`, () => {
+        before(() => enterNewWord(page, Array(letterCount).fill('a').join('')));
+
+        it('shows it on two lines', async () => {
+          const letters = await page.$$('div.row');
+          assert.equal(letters.length, 2);
+        });
+      });
+    });
+
+    [7, 8, 9, 10, 11, 12].forEach(letterCount => {
+      describe(`when a ${letterCount} letter word is entered`, () => {
+        before(() => enterNewWord(page, Array(letterCount).fill('a').join('')));
+
+        it('shows it on three lines', async () => {
+          const letters = await page.$$('div.row');
+          assert.equal(letters.length, 3);
+        });
+      });
+    });
+
+    [13, 14, 15, 16].forEach(letterCount => {
+      describe(`when a ${letterCount} letter word is entered`, () => {
+        before(() => enterNewWord(page, Array(letterCount).fill('a').join('')));
+
+        it('shows it on four lines', async () => {
+          const letters = await page.$$('div.row');
+          assert.equal(letters.length, 4);
+        });
+      });
+    });
+
+    [17, 18, 19, 20, 30].forEach(letterCount => {
+      describe(`when a ${letterCount} letter word is entered`, () => {
+        before(() => enterNewWord(page, Array(letterCount).fill('a').join('')));
+
+        it('shows it on five lines', async () => {
+          const letters = await page.$$('div.row');
+          assert.equal(letters.length, 5);
+        });
       });
     });
   });
